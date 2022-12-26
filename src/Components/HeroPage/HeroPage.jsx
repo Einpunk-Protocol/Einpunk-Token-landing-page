d751bac0138652e68c679b2491128ad4311d4954
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-26 13:37:54 +0100

    Started Creating Components required for the Landing page

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./HeroPage.css"
 import Line from "../../Assets/images/Line.png"
+import EinpunkIllustration from "../../Assets/images/einpunk_illustration.png"
 
 const HeroPage = () => {
   return (
     <div className='heropage-container'>
         <img src={Line} alt=""  className='line'/>
-        <p>HeroPage</p>
+        <div className='heropage-details'>
+            <div className='heropage-text'>
+                <h2>The World's Fastest Growing Token Protocol</h2>
+            </div>
+            <div className='heropage-image'>
+                <img src={EinpunkIllustration} alt="Einpunk Illustration" />
+            </div>
+        </div>
     </div>
   )
 }
