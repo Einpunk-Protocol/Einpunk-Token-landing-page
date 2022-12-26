b26075ec07da6f8fa1808a36279698c65693f46d
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-26 12:43:48 +0100

    Started Creating Components required for the Landing page

--- src/Components/NavBar.jsx/NavBar.jsx
+++ src/Components/NavBar.jsx/NavBar.jsx
@@ -7,7 +7,14 @@ const NavBar = () => {
     <div className='navbar-container'>
         <div className='navbar-logo'>
             <img src={EinpunkLogo} alt="Einpunk Logo" />
-
+        </div>
+        <div className='menu-list'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>How to buy</li>
+                <button>Buy Now</button>
+            </ul>
         </div>
     </div>
   )
