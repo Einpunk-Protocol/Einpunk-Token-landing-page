c2f383a876a321be77925fe487c540819d4d2c07
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-27 12:00:26 +0100

    Started Working on the HeroPage Component

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -4,6 +4,7 @@ import Line from "../../Assets/images/Line.png"
 import EinpunkIllustration from "../../Assets/images/einpunk illustration.png"
 
 const HeroPage = () => {
+    let url = "https://t.me/+cBzvl0ZWRkxjYmMx"
   return (
     <div className='heropage-container'>
         <img src={Line} alt=""  className='line'/>
@@ -11,10 +12,10 @@ const HeroPage = () => {
             <div className='heropage-text'>
                 <h2>The World's Fastest Growing <br/> Token Protocol</h2>
                 <p>A highly-curated platform for creating, collecting and trading unique <br/>
-NFTs. Trade with confidence on the world’s fastest and most secure <br/>
-crypto exchange
-
-</p>
+                    NFTs. Trade with confidence on the world’s fastest and most secure <br/>
+                crypto exchange
+                </p>
+                <a href={url}>Join our Community</a>
             </div>
             <div className='heropage-image'>
                 <img src={EinpunkIllustration} alt="Einpunk Illustration" />
