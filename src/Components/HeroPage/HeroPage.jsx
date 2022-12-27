59aa8f8103351ded1c1b1d4661f4138c68431198
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-27 12:17:08 +0100

    Started Working on the HeroPage Component

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -11,14 +11,10 @@ const HeroPage = () => {
         <div className='heropage-details'>
             <div className='heropage-text'>
                 <h2>The World's Fastest Growing <br/> Token Protocol</h2>
-                <p>A highly-curated platform for creating, collecting and trading unique <br/>
-                    NFTs. Trade with confidence on the world’s fastest and most secure <br/>
-                crypto exchange
+                <p>Einpunk Token is a unique deflationary token with a 1% slippage, total supply of 1000T, 100% ownership renounced and Liquidity pool locked forever.</p>
+                <p>31% of Einpunk Tokens have been burnt so far.
+                The goal of the project is to establish a NFT club (Einpunk Club), where users can hold, buy and sell unique NFTs on a p2p platform.
                 </p>
-                <p>Einpunk Token is a unique deflationary token with a 1% slippage, total supply of 1000T, 100% ownership renounced and Liquidity pool locked forever.<br/>
-
-31% of Einpunk Tokens have been burnt so far.
-The goal of the project is to establish a NFT club (WikiCat Club), where users can hold, buy and sell unique NFTs on a p2p platform.</p>
                 <a href={url}>Join our Community</a>
             </div>
             <div className='heropage-image'>
