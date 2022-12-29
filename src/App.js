04866101074edcc5dbf3b0407dc5449b34945cee
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-29 12:51:41 +0100

    Routers now working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
  import './App.css';
+ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import HeroPage from './Components/HeroPage/HeroPage';
  import NavBar from './Components/NavBar.jsx/NavBar';
+ import About from './Components/About/About';
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <HeroPage/>
+      <Router>
+        <Routes>
+        <Route path="/" element={<HeroPage/>}/>
+        <Route path="about" element={<About/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
