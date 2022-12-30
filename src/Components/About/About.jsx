7e432a881c177505e67bd364ea5a3aef93286743
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-30 14:46:51 +0100

    Started working on the About Component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,6 +4,11 @@ const About = () => {
   return (
     <div className='about-container'>
         <h3>Einpunk Features</h3>
+        <div className='einpunk-features'>
+            <img src="" alt="" />
+            <h4>Dapps</h4>
+            <p>Track, Discover And Analyze The Latest Dapps</p>
+        </div>
     </div>
   )
 }
