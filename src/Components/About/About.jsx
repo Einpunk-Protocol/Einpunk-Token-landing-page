c21a91fd3a61a10090f3f48eb3a6f1a9ef96617f
Olagboye Seyi <akingbolaola@gmail.com>
2022-12-30 14:49:32 +0100

    Started working on the About Component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./About.css"
+import DappsImage from "../../Assets/images/Dapps1-c-1.png"
 const About = () => {
   return (
     <div className='about-container'>
         <h3>Einpunk Features</h3>
         <div className='einpunk-features'>
-            <img src="" alt="" />
+            <img src={DappsImage} alt="" />
             <h4>Dapps</h4>
             <p>Track, Discover And Analyze The Latest Dapps</p>
         </div>
