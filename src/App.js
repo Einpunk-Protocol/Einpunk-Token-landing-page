a5995d84f4e41e66232cb2f1db7681ed4aea9442
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-02 17:48:36 +0100

    Started working on How to Buy component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
  import './App.css';
  import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import HeroPage from './Components/HeroPage/HeroPage';
+ import HeroPage from './Components/HeroPage/HeroPage';
  import NavBar from './Components/NavBar.jsx/NavBar';
  import About from './Components/About/About';
+import HowToBuy from './Components/HowToBuy/HowToBuy';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Routes>
         <Route exact path="/" element={<HeroPage/>}/>
         <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/howtobuy" element={<HowToBuy/>}/>
         </Routes>
       </Router>
     </div>
