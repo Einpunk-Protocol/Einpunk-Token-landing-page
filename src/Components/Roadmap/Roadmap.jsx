2e889777c80b9836755299cba8c0471444a0f308
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-02 13:17:16 +0100

    Started working on the roadmap component

--- src/Components/Roadmap/Roadmap.jsx
+++ src/Components/Roadmap/Roadmap.jsx
@@ -6,6 +6,7 @@ const Roadmap = () => {
     <div className='roadmap-container'>
         <h4 className='roadmap-text'>Roadmap</h4>
         <p className='roadmap-note'>Note: We are determined to follow through each project listed, within their given timeframes.</p>
+        <div className='roadmap-paletees'>
         <div className='roadmap-paletee'>
             <h5>Phase 1</h5>
             <p>Einpunk Token Launch</p>
@@ -15,6 +16,7 @@ const Roadmap = () => {
             <p>Contract Auditing by solidity finance</p>
             <p>1000 organic telegram members</p>
             <p>Creation of Chinese community</p>
+        </div>
         </div>
     </div>
   )
