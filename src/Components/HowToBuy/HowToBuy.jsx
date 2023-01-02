37393eefb0cf02bbe57e2e5e5de69f04dc187c1f
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-02 18:14:57 +0100

    Started working on How to Buy component

--- src/Components/HowToBuy/HowToBuy.jsx
+++ src/Components/HowToBuy/HowToBuy.jsx
@@ -12,6 +12,9 @@ const HowToBuy = () => {
                <h5><Tag color="#269AFE" className="tag">1</Tag>
                Go to the Apple or Play store and download Trust Wallet or MetaMask.
                </h5>
+               <h5><Tag color="#269AFE" className="tag">2</Tag>
+               Purchase ETH via the app, or deposit to your address.
+               </h5>
             </div>
             <div className='how-to-buy-image'>
                 <img src={howToBuyImage} alt="How to Buy Illustration" />
