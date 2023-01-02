5fe844645dd026ceb4308aae461dc07fd24da81c
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-02 14:55:08 +0100

    Started working on the roadmap component

--- src/Components/Roadmap/Roadmap.jsx
+++ src/Components/Roadmap/Roadmap.jsx
@@ -20,35 +20,31 @@ const Roadmap = () => {
         </div>
         <div className='roadmap-paletee'>
             <h5>Phase 2</h5>
-            <p>Einpunk Token Launch</p>
-            <p>500 holders</p>
+            <p>CoinMarketCap & Coin gecko listing</p>
+            <p>5000 telegram members</p>
             <p>2000 wallet Transactions</p>
-            <p>Website Launch</p>
-            <p>Contract Auditing by solidity finance</p>
-            <p>1000 organic telegram members</p>
-            <p>Creation of Chinese community</p>
+            <p>2000 holders</p>
+            <p>Listing on a centralised exchange</p>
         </div>
         </div>
         <div className='roadmap-paletees'>
         <div className='roadmap-paletee'>
             <h5>Phase 3</h5>
-            <p>Einpunk Token Launch</p>
-            <p>500 holders</p>
-            <p>2000 wallet Transactions</p>
-            <p>Website Launch</p>
-            <p>Contract Auditing by solidity finance</p>
-            <p>1000 organic telegram members</p>
-            <p>Creation of Chinese community</p>
+            <p>20,000 telegram members</p>
+            <p>12,000 holders</p>
+            <p>Large influencer marketing</p>
+            <p>Project review</p>
+            <p>Website redesign</p>
         </div>
         <div className='roadmap-paletee'>
             <h5>Phase 4</h5>
-            <p>Einpunk Token Launch</p>
-            <p>500 holders</p>
-            <p>2000 wallet Transactions</p>
-            <p>Website Launch</p>
-            <p>Contract Auditing by solidity finance</p>
-            <p>1000 organic telegram members</p>
-            <p>Creation of Chinese community</p>
+            <p>70,000 holders</p>
+            <p>100,000 telegram members</p>
+            <p>Launch of Einpunk Market website</p>
+            <p>Launch of Einpunk Dapp</p>
+            <p>Increase marketing</p>
+            <p>Partnership announcement</p>
+            <p>Listing on more Major Top CEX</p>
         </div>
         </div>
     </div>
