06db5d8029c5de31ed8006d5b3124cf0abf2a43a
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-03 16:12:00 +0100

    Started working on the Responsiveness

--- src/Components/NavBar.jsx/NavBar.jsx
+++ src/Components/NavBar.jsx/NavBar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./NavBar.css"
-import { FaAlignRight } from "react-icons/fa";
+import {Drawer} from "antd"
+import { FaAlignRight, FaTimes } from "react-icons/fa";
 import {NavLink} from "react-router-dom"
 import EinpunkLogo from "../../Assets/images/Einpunk Logo white.png"
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
+
+    const showNavBar = () =>{
+        setOpen(true)
+    }
+    const onClose = () => {
+        setOpen(false)
+    }
+    
   return (
     <div className='navbar-container'>
         <div className='navbar-logo'>
@@ -27,8 +37,29 @@ const NavBar = () => {
                 })}>Buy Now</NavLink></li>
                 <button>Connect Wallet</button>
             </ul>
-            <FaAlignRight className='menu-bar'/>
+            <FaAlignRight className='menu-bar' onClick={showNavBar}/>
         </div>
+        <Drawer closable={false} placement='bottom' open={open} className='menu-drawer' onClose={onClose}>
+            <FaTimes />
+            <ul>
+                <li><NavLink to="/" className="nav-links"
+                style={({isActive}) =>({
+                    color: isActive ? "#269AFE" : "#FFF"
+                })}>Home</NavLink></li>
+                <li><NavLink to="/about" className="nav-links" style={({isActive}) =>({
+                    color: isActive ? "#269AFE" : "#FFF"
+                })}>About</NavLink></li>
+                <li><NavLink to="/howtobuy" className="nav-links" style={({isActive}) =>({
+                    color: isActive ? "#269AFE" : "#FFF"
+                })}>How to buy</NavLink></li>
+                <li><NavLink to="/buynow" className="nav-links" style={({isActive}) =>({
+                    color: isActive ? "#269AFE" : "#FFF"
+                })}>Buy Now</NavLink></li>
+                <button>Connect Wallet</button>
+            </ul>
+
+        </Drawer>
+
     </div>
   )
 }
