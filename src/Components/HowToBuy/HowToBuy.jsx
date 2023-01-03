c4496fe8ec5ca761d61e8debf602cfbaefcf1f46
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-03 15:11:19 +0100

    Started working on How to Buy component started adding the how to buy details

--- src/Components/HowToBuy/HowToBuy.jsx
+++ src/Components/HowToBuy/HowToBuy.jsx
@@ -19,10 +19,10 @@ const HowToBuy = () => {
                Go to the DApps tab at the bottom and find PancakeSwap. iPhone users will have to enable the browser within TrustWallet by typing  trust://browser_enable  within Safari.
                </h5>
                <h5><Tag color="#269AFE" className="tag">4</Tag>
-               On Pancake Swap, in the "From" slot select BNB or BSC, depending on which one you bought. In the bottom slot tap "Select currency" to search, and then paste in the contact address on this page
+               On Pancake Swap, in the "From" slot select ETH depending on which one you bought. In the bottom slot tap "Select currency" to search, and then paste in the contact address on this page
                </h5>
                <h5><Tag color="#269AFE" className="tag">5</Tag>
-               On Pancake Swap, in the "From" slot select ETH depending on which one you bought. In the bottom slot tap "Select currency" to search, and then paste in the contact address on this page
+               
                </h5>
             </div>
             <div className='how-to-buy-image'>
