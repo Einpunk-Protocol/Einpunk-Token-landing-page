56692e688e9bb266b1fe16fa1539270c0b862b73
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-03 18:28:39 +0100

    Started working on the Responsiveness

--- src/Components/NavBar.jsx/NavBar.jsx
+++ src/Components/NavBar.jsx/NavBar.jsx
@@ -45,18 +45,18 @@ const NavBar = () => {
                 <li><NavLink to="/" className="nav-links"
                 style={({isActive}) =>({
                     color: isActive ? "#269AFE" : "#FFF"
-                })}>Home</NavLink></li>
-                <li><NavLink to="/about" className="nav-links" style={({isActive}) =>({
+                })} onClick={onClose}>Home</NavLink></li>
+                <li><NavLink to="/about" className="nav-links" onClick={onClose} style={({isActive}) =>({
                     color: isActive ? "#269AFE" : "#FFF"
                 })}>About</NavLink></li>
-                <li><NavLink to="/howtobuy" className="nav-links" style={({isActive}) =>({
+                <li><NavLink to="/howtobuy" className="nav-links" onClick={onClose} style={({isActive}) =>({
                     color: isActive ? "#269AFE" : "#FFF"
                 })}>How to buy</NavLink></li>
-                <li><NavLink to="/buynow" className="nav-links" style={({isActive}) =>({
+                <li><NavLink to="/buynow" className="nav-links" onClick={onClose} style={({isActive}) =>({
                     color: isActive ? "#269AFE" : "#FFF"
                 })}>Buy Now</NavLink></li>
-                <button>Connect Wallet</button>
             </ul>
+            <button>Connect Wallet</button>
 
         </Drawer>
 
