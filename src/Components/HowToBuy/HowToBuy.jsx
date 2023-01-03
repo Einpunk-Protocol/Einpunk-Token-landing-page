0ed2833e29fed1da5238943d90a52e8f842fd3e8
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-03 17:32:24 +0100

    Started working on the Responsiveness

--- src/Components/HowToBuy/HowToBuy.jsx
+++ src/Components/HowToBuy/HowToBuy.jsx
@@ -16,19 +16,19 @@ const HowToBuy = () => {
                Purchase ETH via the app, or deposit to your address.
                </h5>
                <h5><Tag color="#269AFE" className="tag">3</Tag>
-               Go to the DApps tab at the bottom and find PancakeSwap. iPhone users will have to enable the browser within TrustWallet by typing  trust://browser_enable  within Safari.
+               Go to the DApps tab at the bottom and find PancakeSwap. iPhone <br/> users will have to enable the browser within TrustWallet<br/> by typing  trust://browser_enable  within Safari.
                </h5>
                <h5><Tag color="#269AFE" className="tag">4</Tag>
-               On Pancake Swap, in the "From" slot select ETH depending on which one you bought. In the bottom slot tap "Select currency" to search, and then paste in the contact address on this page
+               On Pancake Swap, in the "From" slot select ETH depending on <br/>which one you bought. In the bottom slot tap "Select currency"<br/> to search, and then paste in the contact address on this page
                </h5>
                <h5><Tag color="#269AFE" className="tag">5</Tag>
-               Click the icon with the dials at the top left, then set the slippage tolerance to 11-12%, and close that interface once done.
+               Click the icon with the dials at the top left, then set the<br/> slippage tolerance to 11-12%, and close that interface once <br/>done.
                </h5>
                <h5><Tag color="#269AFE" className="tag">6</Tag>
-               Put the amount of BNB you wish to spend in the "From" box, and press the "Swap" button.
+               Put the amount of BNB you wish to spend in the "From" box, and <br/>press the "Swap" button.
                </h5>
                <h5><Tag color="#269AFE" className="tag">7</Tag>
-               Click the swap button & confirm the transaction … Congratulations you now own Einpunk Token!
+               Click the swap button & confirm the transaction … <br/>Congratulations you now own Einpunk Token!
                </h5>
             </div>
             <div className='how-to-buy-image'>
