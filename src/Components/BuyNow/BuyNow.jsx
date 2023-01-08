2a390971c00458e967f955bde9dca73c03bb1bd5
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-08 14:02:07 +0100

    Started working on the BuyNow Component

--- src/Components/BuyNow/BuyNow.jsx
+++ src/Components/BuyNow/BuyNow.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./BuyNow.css"
+import uniSwapLogo from "../../Assets/images/uniswap.png"
 const BuyNow = () => {
   return (
     <div>
-        
+        <div className='dex-exchanges'>
+            <div className='dex-exchange'>
+                <img src={uniSwapLogo} alt="" />
+                <a href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x89a081999249407c1153e135a2af7d323419a66a">Buy Einpunk Now</a>
+            </div>
+        </div>
     </div>
   )
 }
