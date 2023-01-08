c17f8433c6faabeca6e5b662300ae815451dec25
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-08 15:30:02 +0100

    Changed the Meta description for SEO

--- src/Components/BuyNow/BuyNow.jsx
+++ src/Components/BuyNow/BuyNow.jsx
@@ -12,7 +12,7 @@ const BuyNow = () => {
             </div>
             <div className='dex-exchange'>
                 <img src={pancakeSwapLogo} alt="" />
-                <a href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x89a081999249407c1153e135a2af7d323419a66a">Buy Einpunk Now</a>
+                <a href="https://pancakeswap.finance/swap?chain=eth&outputCurrency=0x89a081999249407C1153e135A2Af7d323419A66A">Buy Einpunk Now</a>
             </div>
             
         </div>
