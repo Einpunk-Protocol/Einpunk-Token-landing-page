b7a705bebb97ea1aed27c9929dae4deb2281a701
Olagboye Seyi <akingbolaola@gmail.com>
2023-01-08 14:12:43 +0100

    Started stylubg the BuyNow Component

--- src/Components/BuyNow/BuyNow.jsx
+++ src/Components/BuyNow/BuyNow.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./BuyNow.css"
 import uniSwapLogo from "../../Assets/images/uniswap.png"
+import pancakeSwapLogo from "../../Assets/images/pancakeswaplogo.png"
 const BuyNow = () => {
   return (
     <div className='buynow-container'>
@@ -10,7 +11,7 @@ const BuyNow = () => {
                 <a href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x89a081999249407c1153e135a2af7d323419a66a">Buy Einpunk Now</a>
             </div>
             <div className='dex-exchange'>
-                <img src={uniSwapLogo} alt="" />
+                <img src={pancakeSwapLogo} alt="" />
                 <a href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x89a081999249407c1153e135a2af7d323419a66a">Buy Einpunk Now</a>
             </div>
             
