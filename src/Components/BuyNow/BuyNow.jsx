d0613551786e314213a2359f63da9fda3dbf70cb
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-08 13:30:24 +0100

    Added other networks and pairs to buy EinpunkUSD

--- src/Components/BuyNow/BuyNow.jsx
+++ src/Components/BuyNow/BuyNow.jsx
@@ -37,9 +37,26 @@ const BuyNow = () => {
                 <img src={pancakeSwapLogo} alt="" />
                 <motion.a
                 whileTap={{scale: 1.1}}
-                 href="https://pancakeswap.finance/swap?chain=bsc&inputCurrency=0xE933f2ea66764af369B3D5Ee7295a0B976387c0c&outputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56">Buy Einpunk Now</motion.a>
-            </div>
-            
+                 href="https://pancakeswap.finance/swap?chain=bsc&inputCurrency=0xE933f2ea66764af369B3D5Ee7295a0B976387c0c&outputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82">Buy Einpunk Now</motion.a>
+            </div> 
+        </div>
+        <h3>Cake/EinpunkUSD</h3>
+        <div className='dex-exchanges'>
+            <div className='dex-exchange'>
+                <img src={pancakeSwapLogo} alt="" />
+                <motion.a
+                whileTap={{scale: 1.1}}
+                 href="https://pancakeswap.finance/swap?chain=bsc&inputCurrency=0xE933f2ea66764af369B3D5Ee7295a0B976387c0c&outputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82">Buy Einpunk Now</motion.a>
+            </div> 
+        </div>
+        <h3>BNB/EinpunkUSD</h3>
+        <div className='dex-exchanges'>
+            <div className='dex-exchange'>
+                <img src={pancakeSwapLogo} alt="" />
+                <motion.a
+                whileTap={{scale: 1.1}}
+                 href="https://pancakeswap.finance/swap?inputCurrency=0xE933f2ea66764af369B3D5Ee7295a0B976387c0c&outputCurrency=BNB">Buy Einpunk Now</motion.a>
+            </div> 
         </div>
     </div>
   )
