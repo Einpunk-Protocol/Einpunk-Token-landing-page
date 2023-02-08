2d0b9552ce91082de912226a2872bb90fc37cc16
Olagboye Seyi <akingbolaola@gmail.com>
2023-02-08 13:26:44 +0100

    Added other networks to by EinpunkUSD

--- src/Components/BuyNow/BuyNow.jsx
+++ src/Components/BuyNow/BuyNow.jsx
@@ -6,7 +6,7 @@ import pancakeSwapLogo from "../../Assets/images/pancakeswaplogo.png"
 const BuyNow = () => {
   return (
     <div className='buynow-container'>
-      <h3>ETH Network</h3>
+      <h3>ETH/EinpunkUSD</h3>
         <div className='dex-exchanges'>
             <div className='dex-exchange'>
                 <img src={uniSwapLogo} alt="" />
@@ -19,6 +19,15 @@ const BuyNow = () => {
                 <motion.a
                 whileTap={{scale: 1.1}}
                  href="https://pancakeswap.finance/swap?chain=eth&outputCurrency=0x89a081999249407C1153e135A2Af7d323419A66A">Buy Einpunk Now</motion.a>
+            </div>
+        </div>
+        <h3>BUSD/EinpunkUSD</h3>
+        <div className='dex-exchanges'>
+            <div className='dex-exchange'>
+                <img src={pancakeSwapLogo} alt="" />
+                <motion.a
+                whileTap={{scale: 1.1}}
+                 href="https://pancakeswap.finance/swap?chain=bsc&inputCurrency=0xE933f2ea66764af369B3D5Ee7295a0B976387c0c&outputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56">Buy Einpunk Now</motion.a>
             </div>
             
         </div>
