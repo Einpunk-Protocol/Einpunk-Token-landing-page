c568824642b7dfd1d9cfbcb386636344e073b9ac
Einpunk Proto Punk <100175732+punkproto@users.noreply.github.com>
2023-06-06 00:58:51 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-const ngrok = require('ngrok');
-(async function() {
-  const url = await ngrok.connect();
-})(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// const react = require('react');
+// (async function() {
+//   const url = await react.connect();
+// })(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
